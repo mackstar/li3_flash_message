b9ee050d3d467d23ac018506385b9b006e4b5c7f
Rich McIntyre <richard.mackstar@gmail.com>
2019-01-20 11:54:33 +0000

    Aligned functionality with PHP 7.1.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,9 +2,10 @@
 
 use li3_flash_message\extensions\storage\FlashMessage;
 use lithium\action\Dispatcher;
+use lithium\aop\Filters;
 
-Dispatcher::applyFilter('_callable', function($self, $params, $chain) {
-	$object = $chain->next($self, $params, $chain);
+Filters::apply(Dispatcher::class, '_callable', function($params, $next) {
+	$object = $next($params);
 
 	if (is_a($object, 'lithium\action\Controller')) {
 		return FlashMessage::bindTo($object);
